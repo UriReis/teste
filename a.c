6aafbd2aa0135a06de93a52445775ce64c93036c
UriReis <uri19926@gmail.com>
2024-12-17 23:33:55 -0300

    teste criado

---
+++ a.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Biblioteca para entrada e saída
+
+int main() {
+    printf("Olá, mundo!\n"); // Exibe a mensagem na tela
+    return 0; // Indica que o programa terminou com sucesso
+}
